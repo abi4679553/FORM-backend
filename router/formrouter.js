20eb1d9b17ada18e235c84f8e15ff0772c044244
abi4679553 <abivengadajalam7708@gmail.com>
2026-07-28 16:24:38 +0530

    update the delete  folder

--- router/formrouter.js
+++ router/formrouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/create-form", createform);
 router.get("/fetch-form",fetchform);
-router.delete("/delete-form/:id",deleteform);
+router.delete("/delete-user/:id",deleteform);
 
 module.exports = router;
